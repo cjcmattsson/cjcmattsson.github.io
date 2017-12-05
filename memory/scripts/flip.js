c935b823dcacf2fdd1ce7978bace02294b54079b
cjcmattsson <cjcmattsson@gmail.com>
2017-12-05 12:48:20 +0100

    Added pictures

--- memory/scripts/flip.js
+++ memory/scripts/flip.js
@@ -1,77 +1,82 @@
-// FLIPPING CARDS
+// VARIABLES
 
 const gameboard = document.querySelector('.gameboard');
+const cards = document.querySelectorAll('.card');
+const restart = document.querySelector('.restart');
+const cardsArray = Array.from(cards);
 
 for (var i = gameboard.children.length; i >= 0; i--) {
   gameboard.appendChild(gameboard.children[Math.random() * i | 0]);
 }
 
-const cards = document.querySelectorAll('.card');
-const restart = document.querySelector('.restart');
-const cardsArray = Array.from(cards);
-
+// FUNCTIONS
 // PUT DATANUMBER IN ARRAY, CHECK IF SAME NUMBER
-
 checkNumber = [];
 complete = [];
 let correct = 0;
 
+function noClick() {
+  if (checkNumber.length === 2) {
+    gameboard.classList.add('pointer-stop');
+    setTimeout(function(){
+      gameboard.classList.remove('pointer-stop');
+    }, 1000)
+  };
+};
+
+function checkCombo() {
+  if (checkNumber.length === 2) {
+    console.log("Two objects in array");
+    if (checkNumber[0] === checkNumber[1]) {
+      complete[0].classList.add('complete');
+      complete[1].classList.add('complete');
+
+      console.log('match!');
+      complete = [];
+      console.log(complete);
+      checkNumber = [];
+      console.log(checkNumber);
+      correct++;
+      console.log(correct);
+
+
+    } else {
+      setTimeout(function() {
+        console.log('hej');
+        complete[0].classList.remove('flipped-card');
+        complete[1].classList.remove('flipped-card');
+        checkNumber = [];
+        complete = [];
+
+      }, 1000)
+    };
+};
+};
+
+
 cardsArray.forEach((card) => {
   card.addEventListener('click', (event) => {
     card.classList.toggle('flipped-card');
-    card.classList.toggle('flipped-card-p');
 
     console.log(event.target.dataset.number);
     checkNumber.push(event.target.dataset.number);
     complete.push(card);
     console.log(complete);
-    function pause() {
-      if (checkNumber.length === 2) {
-        gameboard.classList.add('pointer-stop');
-        setTimeout(function(){
-          gameboard.classList.remove('pointer-stop');
-        }, 1000)
-      }
-      }
-      pause();
 
-    if (checkNumber.length === 2) {
-      console.log("Two objects in array");
-      if (checkNumber[0] === checkNumber[1]) {
-        complete[0].classList.add('complete');
-        complete[1].classList.add('complete');
+      checkCombo();
+      noClick();
 
-        console.log('match!');
-        complete = [];
-        console.log(complete);
-        checkNumber = [];
-        console.log(checkNumber);
-        correct++;
-        console.log(correct);
-
-        // WHEN YOU'VE COMPLETED GAME
-        if (correct === 8) {
-          setTimeout(function() {
-            alert('GREAT SUCCES!')
-            // cardSuccesBox.style.display = "block";
-            // const cardSuccesBox = document.querySelector('.cardSucces');
-          }, 1000)
-        }
-
-      } else {
+      // WHEN YOU'VE COMPLETED GAME
+      if (correct === 8) {
         setTimeout(function() {
-          console.log('hej');
-          complete[0].classList.remove('flipped-card');
-          complete[0].classList.remove('flipped-card-p');
-          complete[1].classList.remove('flipped-card');
-          complete[1].classList.remove('flipped-card-p');
-          checkNumber = [];
-          complete = [];
-
+          alert('GREAT SUCCES!')
+          // cardSuccesBox.style.display = "block";
+          // const cardSuccesBox = document.querySelector('.cardSucces');
         }, 1000)
-      }
-    };
-  });
+      };
+
+    });
+
 
 
 
